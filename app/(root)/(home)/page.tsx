133fe8b38010bdbc8dbc17bba46250b3d4eab560
cpshibinoppo <cpshibinoppo@gmail.com>
2024-04-03 09:40:55 +0530

    hero section over

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import Heading from "@/components/Heading";
 
 const Home = () => {
+  const now = new Date();
+  const time = now.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat([], {
+    dateStyle: "full",
+  }).format(now);
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
@@ -10,10 +18,8 @@ const Home = () => {
             Upcoming Meeting at: 12:30 PM
           </h2>
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">11:30 AM</h1>
-            <p className="text-lg font-medium text-sky-1 lg:text-2xl">
-              Saturday, March 23, 2024
-            </p>
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
           </div>
         </div>
       </div>
